8f2295b708af6b97f44437645e0a10019ba8f630
Gal Egozi <geegozi@gmail.com>
2026-01-05 16:47:34 -0600

    Saving the current changes. Adding some of the necessary variables.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function displayValue() {
-    // 1. Get the value from the input field
-    const inputField = document.getElementById("userInput");
-    const value = inputField.value;
+function computeDecayConstant() {
+    const elemName = document.getElementById("elementName").value;
+    const isotope = document.getElementById("isotope").value;
+    const energy = document.getElementById("energy").value;
 
     // 2. Target the display area
     const displayArea = document.getElementById("displayArea");
 
     // 3. Print the value back to the page
-    displayArea.innerText = "You entered: " + value;
+    // displayArea.innerText = "You entered: " + value;
+
+    displayArea.innerText = `The decay constant for element ${elemName} with isotope ${isotope} and an energy of ${energy} is: 999.999`
 }
