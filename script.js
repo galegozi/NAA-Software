0c61e1bd444a3e4cbda699aa01155bb6ad9dc8e2
Gal Egozi <geegozi@gmail.com>
2026-01-05 17:28:53 -0600

    Adding improvements to setup the half life and decay constant.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function computeDecayConstant() {
+function computeDecayConstant(halfLife) {
+    //argument half life: the half life in seconds.
+    return Math.log(2) / halfLife
+}
+
+function submitButtonPress() {
     const elemName = document.getElementById("elementName").value;
     const isotope = document.getElementById("isotope").value;
     const energy = document.getElementById("energy").value;
+
+    alert("Using energy for decay constant. Please fix this & remove this message.");
+
+    const decayConstant = computeDecayConstant(energy);
 
     // 2. Target the display area
     const displayArea = document.getElementById("displayArea");
@@ -9,5 +18,7 @@ function computeDecayConstant() {
     // 3. Print the value back to the page
     // displayArea.innerText = "You entered: " + value;
 
-    displayArea.innerText = `The decay constant for element ${elemName} with isotope ${isotope} and an energy of ${energy} is: 999.999`
+    //decay constant = ln2/half time
+
+    displayArea.innerText = `The decay constant for element ${elemName} with isotope ${isotope} and an energy of ${energy} is: ${decayConstant}`;
 }
