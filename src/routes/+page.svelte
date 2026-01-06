396b49c2f06ecb9669b50f0d49e4504d46897224
Gal Egozi <8800898+galegozi@users.noreply.github.com>
2026-01-05 23:31:08 -0600

    Adding styling & existing functionality back. (#5)
    * Trying to deploy base Sveltekit app
    
    * Adding back existing functionality in Svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
-<h1>Welcome to your library project</h1>
+<script lang="ts">
+	let elementName = $state('');
+	let isotopeName = $state('');
+	let energy = $state(0);
+	let halfLife = $state(0);
+	function computeDecayConstant(hl: number) {
+		//argument hl: the half life in seconds.
+		return Math.log(2) / hl;
+	}
+    let decayConstant = $derived(computeDecayConstant(halfLife))
+</script>
+
+<div style="padding: 5%">
+	<label class="label">
+		<span>Element Name:</span>
+		<input class="input w-50" type="text" bind:value={elementName} />
+	</label>
+	<label class="label">
+		<span>Isotope:</span>
+		<input class="input w-50" type="text" bind:value={isotopeName} />
+	</label>
+	<label class="label">
+		<span>Energy (in KeV):</span>
+		<input class="input w-50" type="number" bind:value={energy} />
+	</label>
+	<label class="label">
+		<span>Half Life (in seconds, s):</span>
+		<input class="input w-50" type="number" bind:value={halfLife} />
+	</label>
+    <br/><br/>
+    <p>The decay constant is: {decayConstant}</p>
+</div>
+
+<!-- <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
