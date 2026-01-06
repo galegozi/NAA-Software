b1f02a6a7f1070d3ba233a2513ac57770f315e1d
Gal Egozi <geegozi@gmail.com>
2026-01-06 15:01:56 -0600

    Adding  material pages & mass correction factor.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,107 @@
-<script lang="ts">
-	let elementName = $state('');
-	let isotopeName = $state('');
-	let energy = $state(0);
-	let halfLife = $state(0);
-	function computeDecayConstant(hl: number) {
-		//argument hl: the half life in seconds.
-		return Math.log(2) / hl;
-	}
-    let decayConstant = $derived(computeDecayConstant(halfLife))
+<script>
+	import IsotopeInfo from './inputs/isotopeInfo.svelte';
+	import MaterialInfo from './inputs/materialInfo.svelte';
+
+	let step = $state(1);
+
+	let isoRef = $state();
+	let refMatRef = $state();
+	let unknownMatRef = $state();
+
+	let isotopeInfo = $state({});
+	let materials = $state({
+		reference: {},
+		unknown: {}
+	});
+
+	const stepExit = (
+		/** @type {{ (): number; (): void; }} */ exitFxn,
+		/** @type {() => boolean} */ validateStep
+	) => {
+		if (!validateStep()) {
+			alert('Please fill out all fields correctly before proceeding.');
+			return;
+		}
+		exitFxn();
+	};
+
+	const next = () => step++;
+	const prev = () => step--;
+
+	const handleSubmit = () => {
+		//validate and go to step 4
+		//for now, just display info.
+		alert(
+			`Isotope Info: ${JSON.stringify(
+				isotopeInfo
+			)}\nReference Material Info: ${JSON.stringify(
+				materials.reference
+			)}\nUnknown Material Info: ${JSON.stringify(materials.unknown)}`
+		);
+		alert(`The mass ratio is: ${
+			(materials.reference.mass / materials.unknown.mass)
+		}`);
+	};
 </script>
 
 <div style="padding: 5%">
-	<label class="label">
-		<span>Element Name:</span>
-		<input class="input w-50" type="text" bind:value={elementName} />
-	</label>
-	<label class="label">
-		<span>Isotope:</span>
-		<input class="input w-50" type="text" bind:value={isotopeName} />
-	</label>
-	<label class="label">
-		<span>Energy (in KeV):</span>
-		<input class="input w-50" type="number" bind:value={energy} />
-	</label>
-	<label class="label">
-		<span>Half Life (in seconds, s):</span>
-		<input class="input w-50" type="number" bind:value={halfLife} />
-	</label>
-    <br/><br/>
-    <p>The decay constant is: {decayConstant}</p>
-</div>
+	<form
+		onsubmit={(e) => {
+			e.preventDefault();
+			handleSubmit();
+		}}
+	>
+		{#if step === 1}
+			<p>Step 1: Isotope Information</p>
+			<p>
+				This is where you enter information about the isotope. This is used in the concentration
+				calculations.
+			</p>
+			<IsotopeInfo bind:this={isoRef} bind:isotopeInfo />
 
-<!-- <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+			<button type="button" onclick={() => stepExit(next, isoRef.validateIsotopeInfo)}>
+				Next
+			</button>
+		{:else if step === 2}
+			<p>Step 2: Reference Material Information</p>
+			<p>
+				This is where you enter information about the reference material. This is used when
+				comparing to the unknown material to determine concentrations.
+			</p>
+			<MaterialInfo bind:this={refMatRef} bind:materialInfo={materials.reference} />
+			<button type="button" onclick={() => stepExit(prev, refMatRef.validateMaterialInfo)}>
+				Back
+			</button>
+			&nbsp;&nbsp;
+			<button type="button" onclick={() => stepExit(next, refMatRef.validateMaterialInfo)}>
+				Next
+			</button>
+		{:else if step === 3}
+			<p>Step 3: Unknown Material Information</p>
+			<p>
+				This is where you enter information about the unknown material you are trying to understand.
+			</p>
+			<MaterialInfo bind:this={unknownMatRef} bind:materialInfo={materials.unknown} />
+			<button type="button" onclick={() => stepExit(prev, unknownMatRef.validateMaterialInfo)}>
+				Back
+			</button>
+			&nbsp;&nbsp;
+			<button type="submit">Submit</button>
+		{:else if step === 4}
+			<p>Step 4: Review and Submit</p>
+			<p>Please review your information before submitting.</p>
+			<h3>Isotope Information</h3>
+			<pre>{JSON.stringify(isotopeInfo, null, 2)}</pre>
+			<h3>Reference Material Information</h3>
+			<pre>{JSON.stringify(materials.reference, null, 2)}</pre>
+			<h3>Unknown Material Information</h3>
+			<pre>{JSON.stringify(materials.unknown, null, 2)}</pre>
+			<button type="button" onclick={prev}>Back</button>
+			&nbsp;&nbsp;
+			<button type="submit">Submit</button>
+		{/if}
+		<br />
+		<!-- {#if step > 2}<button type="button" onclick={prev}>Back</button>{/if} -->
+		<!-- {#if step == 2}<button type="button" onclick={next}>Next</button>{/if} -->
+	</form>
+</div>
---
+++ src/routes/inputs/materialInfo.svelte
@@ -0,0 +1,30 @@
+<!--Put information about materials here (both known and unknown)-->
+
+<script lang="ts">
+	let {
+		materialInfo = $bindable({
+			NETL_code: '',
+			sampleName: '',
+			mass: 0,
+		})
+	} = $props();
+
+	export function validateMaterialInfo() {
+		return materialInfo.NETL_code !== ''
+			&& materialInfo.sampleName !== ""
+			&& materialInfo.mass !== 0;
+	}
+</script>
+
+<label class="label">
+	<span>NETL Code</span>
+	<input class="input w-50" type="text" bind:value={materialInfo.NETL_code} />
+</label>
+<label class="label">
+	<span>Sample Name</span>
+	<input class="input w-50" type="text" bind:value={materialInfo.sampleName} />
+</label>
+<label class="label">
+	<span>Mass (in grams, g)</span>
+	<input class="input w-50" type="number" bind:value={materialInfo.mass} />
+</label>
