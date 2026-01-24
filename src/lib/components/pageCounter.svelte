91a684e2503a89fc26f30aafe05afc07e4acf808
Gal Egozi <geegozi@gmail.com>
2026-01-23 18:47:47 -0700

    Adding a new pageCounter component & refactoring the code.

---
+++ src/lib/components/pageCounter.svelte
@@ -0,0 +1,29 @@
+<!--This component handles initializing the number of pages of a specific type to be used.-->
+<!--isotopes are used here, but it should be generic. It should take a string, as well as a variable that it can access in +page.svelte-->
+<script lang="ts">
+	let { pageCount = 1, updateFxn, pageType } = $props();
+</script>
+
+<label class="label">
+	<span>How many {pageType} do you want to analyze?</span>
+	<input
+		class="input w-20"
+		type="number"
+		min="1"
+		bind:value={pageCount}
+		onchange={() => {
+			if (!Number.isInteger(pageCount)) {
+				alert(
+					`You did not enter an integer. Using the floor of the value. ${pageType} count = ` +
+						Math.floor(pageCount)
+				);
+				pageCount = Math.floor(pageCount);
+			}
+			if (pageCount < 1) {
+				alert(`Please enter a positive integer for the number of ${pageType}. Using 1 ${pageType}.`);
+				pageCount = 1;
+			}
+            updateFxn(pageCount);
+		}}
+	/>
+</label>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import IsotopeInfo from '$lib/components/isotopeInfo.svelte';
 	import MaterialInfo from '$lib/components/materialInfo.svelte';
 	import RefMatInfo from '$lib/components/refMatInfo.svelte';
+	import PageCounter from '$lib/components/pageCounter.svelte';
 
 	import { getAll as isoGA } from '../lib/NAAMath/isotopeMath.ts';
 	import { getAll as matGA } from '../lib/NAAMath/MaterialMath.ts';
@@ -10,6 +11,7 @@
 	import { getAll as EGA } from '../lib/NAAMath/everythingMath.ts';
 
 	function findIndex(roiData: object[]): number[] {
+		// match each isotope to the closest roi centroid
 		let indices: number[] = [];
 		for (let i = 0; i < isotopeInfo.length; i++) {
 			let isotopeEnergy = isotopeInfo[i].energy;
@@ -28,17 +30,66 @@
 		return indices;
 	}
 
+	function updateIsotopeData(newCount: number) {
+		isotopeCount = newCount;
+		isotopeInfo = Array.from({ length: isotopeCount }, () => ({
+			elementName: '',
+			isotopeName: '',
+			energy: 0,
+			halfLife: 0
+		}));
+		isoRef = Array.from({ length: isotopeCount }, () => undefined);
+
+		materials.reference.counts = Array.from({ length: isotopeCount }, () => ({
+			grossCounts: 0,
+			netCounts: 0,
+			uncertainty: 0
+		}));
+		for (let i = 0; i < materials.unknown.length; i++) {
+			materials.unknown[i].counts = Array.from({ length: isotopeCount }, () => ({
+				grossCounts: 0,
+				netCounts: 0,
+				uncertainty: 0
+			}));
+		}
+	}
+
+	function updateUnknownData(newCount: number) {
+		unknownCount = newCount;
+		matRefs.unknown = Array.from({ length: unknownCount }, () => undefined);
+		materials.unknown = Array.from({ length: unknownCount }, () => ({
+			NETL_code: '',
+			sampleName: '',
+			mass: 0,
+			irradiationTime: 0,
+			decayTime: 0,
+			liveTime: 0,
+			realTime: 0,
+			fluence: 0,
+			counts: Array.from({ length: isotopeCount }, () => ({
+				grossCounts: 0,
+				netCounts: 0,
+				uncertainty: 0
+			})),
+			dtType: undefined
+		}));
+	}
+
 	let step = $state(0);
 
+	// isottope information
 	let isoIndex = $derived(step - 2);
-	let isotopeCount = $state(0);
+	let isotopeCount = $state(1);
+	// holds the reference to each isotope info component
 	let isoRef: (IsotopeInfo | undefined)[] = $state([]);
+	// array of isotope information
 	let isotopeInfo: {
 		elementName: string;
 		isotopeName: string;
 		energy: number;
 		halfLife: number;
 	}[] = $state([]);
+	// computed isotope information
 	let isoComp = $derived(isotopeInfo.map(isoGA));
 
 	//step 1 : number of isotopes
@@ -48,7 +99,7 @@
 	//step 4 + isotope count to 3 + isotope count + unknownCount : unknown material information
 	//step 4 + isotope count + unknownCount : review
 	let unknownIdx = $derived(step - (4 + isotopeCount));
-	let unknownCount = $state(0);
+	let unknownCount = $state(1);
 	let matRefs = $state({
 		reference: undefined as RefMatInfo | undefined,
 		unknown: [] as (MaterialInfo | undefined)[]
@@ -65,11 +116,11 @@
 			realTime: 0,
 			fluence: 0,
 			counts: [] as { grossCounts: number; netCounts: number; uncertainty: number }[],
-			dtType: undefined,
+			dtType: undefined as 'short' | 'simple' | 'mixed' | undefined,
 
 			// specific to Reference Material
-			knownConcentration: [],
-			knownUncertainty: []
+			knownConcentration: [] as number[],
+			knownUncertainty: [] as number[]
 		},
 		unknown: [] as {
 			NETL_code: string;
@@ -81,7 +132,7 @@
 			realTime: number;
 			fluence: number;
 			counts: { grossCounts: number; netCounts: number; uncertainty: number }[];
-			dtType: 'live' | 'real' | undefined;
+			dtType: 'short' | 'simple' | 'mixed' | undefined;
 		}[]
 	});
 	let matComp = $derived({
@@ -94,11 +145,7 @@
 			unknown: materials.unknown.map((unk) => matIsoGA(unk, iso, index))
 		}))
 	);
-	// let multiMatComp = $derived(MMGA(materials.reference, materials.unknown));
 	let multiMatComp = $derived(materials.unknown.map((unk) => MMGA(materials.reference, unk)));
-	// let everythingComp = $derived(
-	// 	isotopeInfo.map((iso, index) => EGA(materials.reference, materials.unknown, iso, index))
-	// );
 	let everythingComp = $derived(
 		isotopeInfo.map((iso, index) =>
 			materials.unknown.map((unk) => EGA(materials.reference, unk, iso, index))
@@ -106,22 +153,19 @@
 	);
 
 	let nextButtonText = $derived(
-		step < 2 + isotopeCount
-			? 'Next'
-			: step === 2 + isotopeCount
-				? 'Next'
-				: unknownIdx < unknownCount - 1
-					? 'Next Unknown'
-					: 'Review All Information'
+		step < 1
+			? 'Next: Number of Isotopes'
+			: step >= 1 && step < 1 + isotopeCount
+				? 'Next: Isotope Information'
+				: step === 1 + isotopeCount
+					? // ? 'Next: Number of Unknowns'
+						'Next: Reference Material Information'
+					: step === 2 + isotopeCount
+						? 'Next: Number of Unknown Materials'
+						: step > 2 + isotopeCount && step < 3 + isotopeCount + unknownCount
+							? 'Next: Unknown Material Information'
+							: 'Review All Information'
 	);
-
-	// const stepExit = (exitFxn: () => void, validateStep: () => boolean) => {
-	// 	if (!validateStep()) {
-	// 		alert('Please fill out all fields correctly before proceeding.');
-	// 		return;
-	// 	}
-	// 	exitFxn();
-	// };
 
 	const next = () => step++;
 	const prev = () => step--;
@@ -130,12 +174,11 @@
 </script>
 
 <svelte:head>
-  <title>NAA Analysis</title>
+	<title>NAA Analysis</title>
 </svelte:head>
 
-
 <div style="padding: 5%">
-	<h1 class="text-3xl font-bold">NAA Analysis - Version 4.1 BETA</h1>
+	<h1 class="text-3xl font-bold">NAA Analysis - Version 4.1.1 BETA</h1>
 	<br />
 	<form
 		onsubmit={(e) => {
@@ -152,11 +195,18 @@
 			<br />
 			<p>Multiple isotopes and unknowns are in beta.</p>
 			<br />
-			<p>The mixed dead time correction is deprecated. The simple correction option to replace it is currently in beta.</p>
+			<p>
+				The mixed dead time correction is deprecated. The simple correction option to replace it is
+				currently in beta.
+			</p>
+			<br />
+			<p>Version 4.1.1 is a refactor to improve code organization and maintainability.</p>
 			<br />
 			<h2 class="text-2xl font-bold">Future plans:</h2>
 			<ol class="list-inside list-decimal">
-				<li>Version 4.2: Improved reporting with table at top. Should include units for concentration.</li>
+				<li>
+					Version 4.2: Improved reporting with table at top. Should include units for concentration.
+				</li>
 			</ol>
 			<br />
 			<h2 class="text-2xl font-bold">Future additions, not planned yet:</h2>
@@ -173,52 +223,8 @@
 			<button type="button" onclick={next}>Get Started</button>
 		{:else if step === 1}
 			<h2 class="text-2xl font-bold">Step 1: Number of Isotopes</h2>
-			<label class="label">
-				<span>How many isotopes do you want to analyze?</span>
-				<input
-					class="input w-20"
-					type="number"
-					min="1"
-					bind:value={isotopeCount}
-					onchange={() => {
-						// if isotopeCount is not an integer, use the floor function & alert.
-						if (!Number.isInteger(isotopeCount)) {
-							alert(
-								'You did not enter an integer. Using the floor of the value. Isotope count = ' +
-									Math.floor(isotopeCount)
-							);
-							isotopeCount = Math.floor(isotopeCount);
-						}
-						if (isotopeCount < 1) {
-							alert('Please enter a positive integer for the number of isotopes. Using 1 isotope.');
-							isotopeCount = 1;
-						}
-						isotopeInfo = Array.from({ length: isotopeCount }, () => ({
-							elementName: '',
-							isotopeName: '',
-							energy: 0,
-							halfLife: 0
-						}));
-						isoRef = Array.from({ length: isotopeCount }, () => undefined);
-
-						//change material
-						materials.reference.counts = Array.from({ length: isotopeCount }, () => ({
-							grossCounts: 0,
-							netCounts: 0,
-							uncertainty: 0
-						}));
-						// For unknowns, need to update each unknown's counts array
-						for (let i = 0; i < materials.unknown.length; i++) {
-							materials.unknown[i].counts = Array.from({ length: isotopeCount }, () => ({
-								grossCounts: 0,
-								netCounts: 0,
-								uncertainty: 0
-							}));
-						}
-					}}
-				/>
-			</label>
-			<button type="button" onclick={next}> Next </button>
+			<PageCounter pageType="isotopes" pageCount={isotopeCount} updateFxn={updateIsotopeData} />
+			<button type="button" onclick={next}> {nextButtonText} </button>
 		{:else if isoIndex >= 0 && isoIndex < isotopeCount}
 			<!--For each step, show this. All of this should be in step 2, but there should be an indication of which isotope is being filled out. Ensure that the forward and back buttons work correctly.-->
 			<!-- {#each { length: isotopeCount } as _, index} -->
@@ -237,7 +243,7 @@
 
 			<button type="button" onclick={prev}> Back </button>
 			&nbsp;&nbsp;
-			<button type="button" onclick={next}> Next </button>
+			<button type="button" onclick={next}> {nextButtonText} </button>
 			<br /><br />
 			<!-- {/each} -->
 		{:else if step === 2 + isotopeCount}
@@ -267,52 +273,13 @@
 
 			<button type="button" onclick={prev}> Back </button>
 			&nbsp;&nbsp;
-			<button type="button" onclick={next}> Next </button>
+			<button type="button" onclick={next}> {nextButtonText} </button>
 		{:else if step === 3 + isotopeCount}
 			<h2 class="text-2xl font-bold">Step {step}: Number of Unknown Materials</h2>
-			<label class="label">
-				<span>How many unknown materials do you want to analyze?</span>
-				<input
-					class="input w-20"
-					type="number"
-					min="1"
-					bind:value={unknownCount}
-					onchange={() => {
-						// if unknownCount is not an integer, use the floor function & alert.
-						if (!Number.isInteger(unknownCount)) {
-							alert(
-								'You did not enter an integer. Using the floor of the value. Unknown count = ' +
-									Math.floor(unknownCount)
-							);
-							unknownCount = Math.floor(unknownCount);
-						}
-						if (unknownCount < 1) {
-							alert('Please enter a positive integer for the number of unknowns. Using 1 unknown.');
-							unknownCount = 1;
-						}
-						matRefs.unknown = Array.from({ length: unknownCount }, () => undefined);
-						materials.unknown = Array.from({ length: unknownCount }, () => ({
-							NETL_code: '',
-							sampleName: '',
-							mass: 0,
-							irradiationTime: 0,
-							decayTime: 0,
-							liveTime: 0,
-							realTime: 0,
-							fluence: 0,
-							counts: Array.from({ length: isotopeCount }, () => ({
-								grossCounts: 0,
-								netCounts: 0,
-								uncertainty: 0
-							})),
-							dtType: undefined
-						}));
-					}}
-				/>
-			</label>
+			<PageCounter pageType="unknown materials" pageCount={unknownCount} updateFxn={updateUnknownData} />
 			<button type="button" onclick={prev}> Back </button>
 			&nbsp;&nbsp;
-			<button type="button" onclick={next}> Next </button>
+			<button type="button" onclick={next}> {nextButtonText} </button>
 		{:else if unknownIdx >= 0 && unknownIdx < unknownCount}
 			<h2 class="text-2xl font-bold">
 				Step {step}: Unknown Material Information for Unknown {unknownIdx + 1}
@@ -334,7 +301,6 @@
 			<h3 class="text-xl font-bold">
 				Unknown and Isotope Information for Unknown {unknownIdx + 1}
 			</h3>
-			<!-- <pre>{JSON.stringify(matIsoComp.unknown, null, 4)}</pre> -->
 			<pre>{JSON.stringify(
 					matIsoComp.map((item) => item.unknown[unknownIdx]),
 					null,
@@ -365,24 +331,8 @@
 			<h4 class="text-lg font-semibold">Computed Values that use everything</h4>
 			<pre>{JSON.stringify(everythingComp, null, 4)}</pre>
 			<br />
-			<!-- <p>Reference Material Information</p>
-			<pre>{JSON.stringify(materials.reference, null, 2)}</pre>
-			<p>
-				Reference Material Saturation Factor: {matComputedFactors.reference.saturationFactor}
-			</p>
-			<p>Unknown Material Information</p>
-			<pre>{JSON.stringify(materials.unknown, null, 2)}</pre>
-			<p>
-				Unknown Material Saturation Factor: {matComputedFactors.unknown.saturationFactor}
-			</p>
-			<p>Mass Ratio</p> -->
-			<!-- <pre>{(correctionFactors as any).mass}</pre> -->
 			<button type="button" onclick={prev}>Back</button>
-			<!-- &nbsp;&nbsp; -->
-			<!-- <button type="submit">Submit</button> -->
 		{/if}
 		<br />
-		<!-- {#if step > 2}<button type="button" onclick={prev}>Back</button>{/if} -->
-		<!-- {#if step == 2}<button type="button" onclick={next}>Next</button>{/if} -->
 	</form>
 </div>
