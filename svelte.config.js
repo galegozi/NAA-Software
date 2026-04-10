50a762dbb2889fbf7e5c245281dbdfb2aef64b8f
Gal Egozi <geegozi@gmail.com>
2026-04-10 17:24:09 -0500

    Trying another fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,53 @@
-// import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// // import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://svelte.dev/docs/kit/integrations
-// 	// for more information about preprocessors
+// // /** @type {import('@sveltejs/kit').Config} */
+// // const config = {
+// // 	// Consult https://svelte.dev/docs/kit/integrations
+// // 	// for more information about preprocessors
+// // 	preprocess: vitePreprocess(),
+
+// // 	kit: {
+// // 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// // 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// // 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// // 		adapter: adapter()
+// // 	}
+// // };
+
+// // export default config;
+
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
 // 	preprocess: vitePreprocess(),
-
 // 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
+// 		appDir: 'app',
+// 		adapter: adapter({
+// 			fallback: '404.html' // Required for single-page app behavior
+// 		}),
+// 		paths: {
+// 			// Replace 'your-repo-name' with your actual repository name
+// 			// base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
+// 			base: ''
+// 		}
 // 	}
 // };
 
-// export default config;
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-	preprocess: vitePreprocess(),
-	kit: {
-		appDir: 'app',
-		adapter: adapter({
-			fallback: '404.html' // Required for single-page app behavior
-		}),
-		paths: {
-			// Replace 'your-repo-name' with your actual repository name
-			// base: process.env.NODE_ENV === 'production' ? '/your-repo-name' : ''
-			base: ''
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    appDir: 'app',
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html'
+    }),
+    paths: {
+      base: ''
+    }
+  }
 };
