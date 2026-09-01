f5e3c15754a908b401ab0ceb5f997ddd1a2741d1
Gal Egozi <geegozi@gmail.com>
2026-09-01 14:33:57 -0500

    Bump APP_VERSION to 7.1 BETA

--- src/lib/utils/stepUtils.ts
+++ src/lib/utils/stepUtils.ts
@@ -5,7 +5,7 @@
  *   0 Welcome -> 1 Select Isotopes -> 2 Build Library -> 3 Unknown Materials -> 4 Review
  */
 
-export const APP_VERSION = '7.1 Developer Preview ALPHA 2';
+export const APP_VERSION = '7.1 BETA';
 
 /**
  * Step type enumeration
